aca58a33e2c4cfc71ed2da184540e48b335ebb82
geoffroy <geoffroy.alarcon@gmail.com>
2021-10-24 15:52:10 +0200

    correction bug

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var voyage = ["Amsterdam", "Londres", "Berlin", "Bruxelles", "Munich", "Madrid", "Florence", "Rome", "Dublin", "Edimbourg"];
 document.getElementById("hasard").addEventListener("click", function () {
     var nombreRamdom = Math.random();
-    var indexRamdom = Math.round(nombreRamdom * 10);
+    var indexRamdom = Math.round(nombreRamdom * 9);
     var destinationchoisie = voyage[indexRamdom];
-    console.log(indexRamdom);
-    document.getElementById("destinationVoyage").value = destinationchoisie;
+   document.getElementById("destinationVoyage").value = destinationchoisie;
 })
