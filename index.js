b3c3272ec2c2ddfd1c973aaa1ea709da3939463d
geoffroy <geoffroy.alarcon@gmail.com>
2021-10-24 15:48:28 +0200

    correction coquille

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var voyage = ["Amsterdam", "Londres", "Berlin", "Bruxels", "Munich", "Madrid", "Florence", "Rome", "Dublin", "Edimbourg"];
+var voyage = ["Amsterdam", "Londres", "Berlin", "Bruxelles", "Munich", "Madrid", "Florence", "Rome", "Dublin", "Edimbourg"];
 document.getElementById("hasard").addEventListener("click", function () {
     var nombreRamdom = Math.random();
     var indexRamdom = Math.round(nombreRamdom * 10);
